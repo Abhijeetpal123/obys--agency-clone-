6d66e95eed29af643c8ed7d60bae6432ea7b41f3
Your Name~ <abhijeetpal0307@gmail.com>
2025-03-31 17:07:09 +0530

    Made Loader Successfully

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var tl = gsap.timeline();
+tl.from(".line h1 ,.line h2", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+
+tl.from("#line1-part1, .line h2",{
+    opacity:0,
+    onStart:function(){
+        var h5 = document.querySelector("#line1-part1 h5");
+        var count = 0;
+        setInterval(function () {
+          if (count < 99) {
+            h5.textContent = count++;
+          } else {
+            count = 100;
+            // h5.innerHTML=count++
+            h5.textContent = count++;
+          }
+        }, 30);
+    }
+})
+tl.to("#loader", {
+  opacity: 0,
+  delay: 4,
+  duration: 0.4,
+});
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    ease:Power4,
+})
+tl.to("#loader",{
+    display:none,
+})
