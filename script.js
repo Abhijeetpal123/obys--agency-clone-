cdb05b24ba92a41fe794efa7d5b1a9eca2360146
Your Name~ <abhijeetpal0307@gmail.com>
2025-04-01 10:57:27 +0530

    Page 1 done

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1 ,.line h2", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-var tl = gsap.timeline();
-tl.from(".line h1 ,.line h2", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from("#line1-part1, .line h2", {
+    opacity: 0,
+    onStart: function () {
+      var h5 = document.querySelector("#line1-part1 h5");
+      var count = 0;
+      setInterval(function () {
+        if (count < 99) {
+          h5.textContent = count++;
+        } else {
+          count = 100;
+          // h5.innerHTML=count++
+          h5.textContent = count++;
+        }
+      }, 30);
+    },
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    delay: 4,
+    duration: 0.4,
+  });
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+  });
+  //   tl.to("#loader", {
+  //     display: none,
+  //   });
+  tl.from("#nav", {
+    y: 120,
+    stagger:0.2,
+  });
+  tl.from("#centre1 h1 , #centre2 h1, #centre3 h2, #centre4 h1", {
+    y: 120,
+    stagger:0.2,
+  });
+}
+loadingAnimation();
 
-tl.from("#line1-part1, .line h2",{
-    opacity:0,
-    onStart:function(){
-        var h5 = document.querySelector("#line1-part1 h5");
-        var count = 0;
-        setInterval(function () {
-          if (count < 99) {
-            h5.textContent = count++;
-          } else {
-            count = 100;
-            // h5.innerHTML=count++
-            h5.textContent = count++;
-          }
-        }, 30);
-    }
-})
-tl.to("#loader", {
-  opacity: 0,
-  delay: 4,
-  duration: 0.4,
-});
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
-    opacity:0,
-    ease:Power4,
-})
-tl.to("#loader",{
-    display:none,
-})
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  Shery.makeMagnet("#nav2 h4");
+}
+cursorAnimation();
